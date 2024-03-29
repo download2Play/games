bdf57a1212bc11dc199c755ba23eaf7ebed09833
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-03-29 11:56:30 +0545

    Create config.js

---
+++ WWE2024/android/config.js
@@ -0,0 +1,6 @@
+const WEB_TITLE = 'WWE 2024 for android';
+const IMAGE_URL = 'https://i.playground.ru/p/pbyeuAjE30R1Ho8buYZIfA.jpeg';
+const GAME_LOGO =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/WWE_official_logo.svg/800px-WWE_official_logo.svg.png';
+
+const OG_ID = '8d9nq3';
