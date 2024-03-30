3471eb3121d629da58e42867d73c3d5ad8420c82
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-03-30 10:02:38 +0545

    Update config.js

--- Final Fantasy VII Rebirth/android/config.js
+++ Final Fantasy VII Rebirth/android/config.js
@@ -1,5 +1,5 @@
 const WEB_TITLE = 'Final Fantasy VII Rebirth for android';
-const IMAGE_URL = 'fyre.cdn.sewest.net/ffvii-hub/6471442498774a5fd66555de/pub106_cloud_zack_sephiroth-3-1--ga4rX0dsG.jpg';
+const IMAGE_URL = 'https://img.opencritic.com/game/15136/qpCPjfO7.jpg';
 const GAME_LOGO =
   'https://static.wikia.nocookie.net/finalfantasy/images/c/c8/Final_Fantasy_VII_Rebirth_logo.png';
 const OG_ID = 'po6165';
