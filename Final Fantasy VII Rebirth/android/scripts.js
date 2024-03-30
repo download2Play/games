3d2e85a89e44f4968f4becf56e88f7cc099f48e6
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-03-30 09:47:48 +0545

    Update scripts.js

--- Final Fantasy VII Rebirth/android/scripts.js
+++ Final Fantasy VII Rebirth/android/scripts.js
@@ -277,7 +277,7 @@ var ogblock = false;
 var contentTitle = document.querySelector('.main-title');
 contentTitle.textContent = WEB_TITLE;
 
-//GAME LOGO
+//GAME LOGO0
 var logoContainer = document.querySelector('.logo-container');
 var logoImage = document.createElement('img');
 logoImage.src = GAME_LOGO; // Set the image source
