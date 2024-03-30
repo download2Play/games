b889b7144d1e1859ddcc9d57cb328451f245dd8a
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-03-30 09:37:02 +0545

    Update config.js

--- Final Fantasy VII Rebirth/android/config.js
+++ Final Fantasy VII Rebirth/android/config.js
@@ -1,5 +1,5 @@
-const WEB_TITLE = 'WWE 2024 for android';
-const IMAGE_URL = 'https://i.playground.ru/p/pbyeuAjE30R1Ho8buYZIfA.jpeg';
+const WEB_TITLE = 'Final Fantasy VII Rebirth for android';
+const IMAGE_URL = 'fyre.cdn.sewest.net/ffvii-hub/6471442498774a5fd66555de/pub106_cloud_zack_sephiroth-3-1--ga4rX0dsG.jpg?quality=85&width=3840';
 const GAME_LOGO =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/WWE_official_logo.svg/800px-WWE_official_logo.svg.png';
-const OG_ID = '8d9nq3';
+  'https://static.wikia.nocookie.net/finalfantasy/images/c/c8/Final_Fantasy_VII_Rebirth_logo.png/revision/latest?cb=20220616223742';
+const OG_ID = 'po6165';
