01b7acb72bba28dbd883c3ce7b83fabbf217afb6
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-04-02 15:22:47 +0545

    Delete alone_in_the_dark directory

--- alone_in_the_dark/android/german/config.js
+++
@@ -1,6 +0,0 @@
-
-    const WEB_TITLE = "Alone in the Dark";
-    const IMAGE_URL = "https://cdn.akamai.steamstatic.com/steam/apps/1310410/capsule_616x353.jpg?t=1711546287";
-    const GAME_LOGO = "https://eu.store.thqnordic.com/media/image/47/c4/2d/Unbenanntes_Projekt.png";
-    const OG_ID = "37285v";
-    
