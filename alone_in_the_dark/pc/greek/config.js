2475c490312b5e5667a21fe7d493894eef9bf2e0
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-02 22:38:07 +0545

    Automated commit via script

---
+++ alone_in_the_dark/pc/greek/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Alone in the Dark";
+    const IMAGE_URL = "https://upload.wikimedia.org/wikipedia/en/0/05/Alone_in_the_dark_2022.jpg";
+    const GAME_LOGO = "https://cdn.akamai.steamstatic.com/steam/apps/548090/header.jpg?t=1696413961";
+    const OG_ID = "7dl8gk";
+    
