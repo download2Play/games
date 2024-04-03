6250a8da1cf10ad82870d5f924179e63a3f4c60f
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-03 14:01:56 +0545

    Automated commit via script

--- dragons_dogma_2/android/french/config.js
+++ dragons_dogma_2/android/french/config.js
@@ -2,5 +2,5 @@
     const WEB_TITLE = "Dragons Dogma 2";
     const IMAGE_URL = "https://upload.wikimedia.org/wikipedia/en/c/c7/Dragon%27s_Dogma_2_cover_art.jpg";
     const GAME_LOGO = "https://assets-prd.ignimgs.com/2024/02/22/dragons-dogma-2-preorder-1708617250170.png";
-    const OG_ID = "None";
+    const OG_ID = "dvrgpk";
     
