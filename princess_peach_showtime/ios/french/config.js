444511c2277eef54a20f1f0e986c8e328335770d
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-03 14:41:10 +0545

    Automated commit via script

---
+++ princess_peach_showtime/ios/french/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Princess Peach Showtime";
+    const IMAGE_URL = "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2024/02/princesspeachshowtimepreview.jpg";
+    const GAME_LOGO = "https://assetsio.gnwcdn.com/Peach-showtime-update.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp";
+    const OG_ID = "42edv5";
+    
