808c1001549ff6ba708477b7ae51ad46a32c287a
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-03 14:20:58 +0545

    Automated commit via script

---
+++ other_notable_games_of_march_2024/android/german/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "OTHER NOTABLE GAMES OF MARCH 2024";
+    const IMAGE_URL = "https://i.ytimg.com/vi/cNdl59xZMHY/maxresdefault.jpg";
+    const GAME_LOGO = "https://staticg.sportskeeda.com/editor/2024/02/cdfe5-17091345955989-1920.jpg?w=840";
+    const OG_ID = "vo5k42";
+    
