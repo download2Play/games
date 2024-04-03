7a24ea365f3d6f125474546f2dd235d0bff9f30e
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-03 15:54:13 +0545

    Automated commit via script

---
+++ rise_of_the_ronin/ios/german/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Rise of the Ronin";
+    const IMAGE_URL = "https://m.media-amazon.com/images/M/MV5BZTJlMGM3ZjctYjJkNy00NWIzLTgwYWMtMWNjYmYzYWI2ODkyXkEyXkFqcGdeQXVyMTk2OTAzNTI@._V1_.jpg";
+    const GAME_LOGO = "https://cdn.entries.clios.com/styles/clio_aotw_ems_image_details_retina/s3/entry_attachments/image/74/2459/42358/147303/T7Uf7zYG4kv8H0X5KTx93GhW-ewHZaNkKpTRrIlWzWk.jpg";
+    const OG_ID = "dvrgwk";
+    
