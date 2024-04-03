c2bc857f8581677723b89ca7e769bab9df743c94
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-03 16:55:44 +0545

    Automated commit via script

---
+++ the_outlast_trials/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "The Outlast Trials";
+    const IMAGE_URL = "https://cdn.mos.cms.futurecdn.net/qFAqRgLTugvvjvcnxyppAj-320-80.jpg";
+    const GAME_LOGO = "https://img.youtube.com/vi/K5C1HlrMtnQ/mqdefault.jpg";
+    const OG_ID = "8d9j1d";
+    
