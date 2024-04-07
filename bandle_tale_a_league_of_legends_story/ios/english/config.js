28a769ad77f9d56db829b2bf1f0cd7f7e12e341b
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-07 18:45:50 +0545

    Automated commit via script

---
+++ bandle_tale_a_league_of_legends_story/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Bandle Tale A League of Legends story";
+    const IMAGE_URL = "https://www.agamingnetwork.com/content/images/size/w1200/2024/01/Copy-of-Bandle_CE_3840x2160_EN-min.png";
+    const GAME_LOGO = "https://cdn.akamai.steamstatic.com/steam/apps/1759380/capsule_616x353.jpg?t=1710190745";
+    const OG_ID = "g6dd4p";
+    
