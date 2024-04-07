0566e415bdf7ce1903ceddd4679e02ce8642a2c9
ranjan nz <ranjan.stha.nz@gmail.com>
2024-04-07 19:11:18 +0545

    Automated commit via script

---
+++ banishers_ghosts_of_new_eden/ios/spanish/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Banishers Ghosts of New Eden";
+    const IMAGE_URL = "https://cdn.akamai.steamstatic.com/steam/apps/1493640/ss_daceff6544b8c053e6336e11f2febfde61ca4cfe.1920x1080.jpg?t=1710229158";
+    const GAME_LOGO = "https://cdn-prod.scalefast.com/public/assets/user/13772687/image/c44ee996e3c744a60639d6c86de07b23.jpg";
+    const OG_ID = "kljjo8";
+    
