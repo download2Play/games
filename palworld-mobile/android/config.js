dcedd236b6adc7fc70ca4f39dc3c2a226d861679
Download2Play <157565339+download2Play@users.noreply.github.com>
2024-04-12 17:15:10 +0545

    Create config.js

---
+++ palworld-mobile/android/config.js
@@ -0,0 +1,5 @@
+const WEB_TITLE = 'Palworld mobile';
+const IMAGE_URL = 'https://apicms.thestar.com.my/uploads/images/2024/01/23/2505709.jpg';
+const GAME_LOGO =
+  'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/9b2ca95a-7874-4fdf-9b22-d90792a9234e/dgstzyw-4f6d1dfc-a81a-4c4b-af8b-d38f3b4641eb.png/v1/fit/w_512,h_512/palworld__2024_video_game__folder_icon__2__by_foldericonboy_dgstzyw-375w-2x.png';
+const OG_ID = '8d9nq3';
