e87c0557694e0fc54a3376d3dcd90e7e1ade45e1
ranjan nz <ranjan.stha.nz@gmail.com>
2024-05-04 18:30:28 +0545

    Automated commit via script

---
+++ foamstars/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Foamstars";
+    const IMAGE_URL = "https://fyre.cdn.sewest.net/foamstars/64ef4f9d98774a5fd67f2efc/soa-artwork-ar924rsOm.png?quality=85&width=3840";
+    const GAME_LOGO = "https://www.gameinformer.com/sites/default/files/styles/product_box_art/public/2024/01/16/d9f819b4/foamstars.jpeg";
+    const OG_ID = "e648xx";
+    
