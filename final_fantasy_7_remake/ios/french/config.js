dea06962ffb975366779d79306a7019b5c561f0c
ranjan nz <ranjan.stha.nz@gmail.com>
2024-05-04 16:48:25 +0545

    Automated commit via script

---
+++ final_fantasy_7_remake/ios/french/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Final Fantasy 7 Remake";
+    const IMAGE_URL = "https://upload.wikimedia.org/wikipedia/en/c/ce/FFVIIRemake.png";
+    const GAME_LOGO = "https://i.pinimg.com/736x/a3/84/58/a384585aaea471a3233a10f99aeed536.jpg";
+    const OG_ID = "j6v86n";
+    
