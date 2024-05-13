960b5532bfe9b65b4e825247d842453ddd1659da
ranjan nz <ranjan.stha.nz@gmail.com>
2024-05-13 07:32:47 +0545

    Automated commit via script

---
+++ granblue_fantasy_relink/ios/french/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Granblue Fantasy Relink";
+    const IMAGE_URL = "https://cdn.oneesports.gg/cdn-data/2024/01/Gaming_GranblueFantasyRelink_KeyArt_3.jpg";
+    const GAME_LOGO = "https://img.youtube.com/vi/r-K5n1sbdrQ/maxresdefault.jpg";
+    const OG_ID = "37q7g7";
+    
