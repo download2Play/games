fd89c654d8adbeea8504026f40198862f19764f0
ranjan nz <ranjan.stha.nz@gmail.com>
2024-05-13 08:17:37 +0545

    Automated commit via script

---
+++ helldivers_2/android/spanish/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Helldivers 2";
+    const IMAGE_URL = "https://assetsio.gnwcdn.com/helldivers-2-heavy-enemies-changes-coming-soon-01.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp";
+    const GAME_LOGO = "https://preview.redd.it/i-made-these-helldivers-2-logo-designs-as-if-they-were-v0-06hqjg2cv1lc1.png?width=640&crop=smart&auto=webp&s=8eb01be1adc46cf475e68c36416b9dd0ec76663a";
+    const OG_ID = "1ovo61";
+    
