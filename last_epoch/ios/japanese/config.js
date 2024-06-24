933bf3876a2cf90277293fbb5a8b30c5c6c0ebad
ranjan nz <ranjan.stha.nz@gmail.com>
2024-06-24 13:18:32 +0545

    Automated commit via script

---
+++ last_epoch/ios/japanese/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Last Epoch";
+    const IMAGE_URL = "https://static.odealo.com/upload/images/game-main/last-epoch-market-mobile_6101216368eab_l.jpg";
+    const GAME_LOGO = "https://images.alphacoders.com/135/1352252.png";
+    const OG_ID = "wo1377";
+    
