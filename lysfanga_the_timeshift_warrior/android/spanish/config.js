946f5eafe69e21a6d21b703b907012ee1c478770
ranjan nz <ranjan.stha.nz@gmail.com>
2024-07-07 18:42:12 +0545

    Automated commit via script

---
+++ lysfanga_the_timeshift_warrior/android/spanish/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Lysfanga The TimeShift Warrior";
+    const IMAGE_URL = "https://cdn1.epicgames.com/spt-assets/8d54973beed94535ba02105189cf1948/lysfanga-ext0a.jpg";
+    const GAME_LOGO = "https://assets-prd.ignimgs.com/2023/06/09/the-1686325552002.jpg";
+    const OG_ID = "wo17dj";
+    
