122b99b8526ad8629195917352170a28816f02cb
Download2play <download2play.help@gmail.com>
2024-09-10 21:14:15 +0545

    Automated commit via script

--- elden_ringg/ios/english/config.js
+++ elden_ringg/ios/english/config.js
@@ -2,5 +2,5 @@
     const WEB_TITLE = "Elden Ringg";
     const IMAGE_URL = "https://p325k7wa.twic.pics/high/elden-ring/elden-ring/08-shadow-of-the-erdtree/elden-ring-expansion-SOTE/00-page-content/ERSOTE-header-mobile.jpg";
     const GAME_LOGO = "https://fbi.cults3d.com/uploaders/26336402/illustration-file/24af9960-997f-4d73-acc0-fb22c3319785/with-fire.png";
-    const OG_ID = "wo1vev";
+    const OG_ID = "po98g3";
     
