623107c02908b17decac556c757dedd6829b0bc0
Bikalpa <bikalpaj@gmail.com>
2024-09-10 20:38:03 +0545

    Automated commit via script

--- mario_vs_donkey_kong/ios/english/config.js
+++ mario_vs_donkey_kong/ios/english/config.js
@@ -2,5 +2,5 @@
     const WEB_TITLE = "Mario vs Donkey Kong";
     const IMAGE_URL = "https://upload.wikimedia.org/wikipedia/en/0/05/Alone_in_the_dark_2022.jpg";
     const GAME_LOGO = "https://cdn.akamai.steamstatic.com/steam/apps/548090/header.jpg?t=1696413961";
-    const OG_ID = "o6lw1r";
+    const OG_ID = "dv165k";
     
