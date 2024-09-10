e27a211018d7c4a1432c3cf68f5ec0bc9ddd4ec0
Download2play <download2play.help@gmail.com>
2024-09-10 21:32:16 +0545

    Automated commit via script

---
+++ nba_2k25/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "NBA 2K25";
+    const IMAGE_URL = "https://twinfinite.net/wp-content/uploads/2024/09/NBA-2K25-Review-A-Championship-Run.jpg";
+    const GAME_LOGO = "https://pbs.twimg.com/media/GM7cvRuXIAATQBB.jpg";
+    const OG_ID = "r7653k";
+    
