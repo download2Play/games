8e7ff94f2d605281c820484c814d141ebb29f5e0
Download2play <download2play.help@gmail.com>
2024-09-11 09:37:57 +0545

    Automated commit via script

---
+++ wwe_2k25/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "WWE 2K25";
+    const IMAGE_URL = "https://gamestore.mt/cdn/shop/files/71mXYABE4vL._AC_SL1500.jpg";
+    const GAME_LOGO = "https://i.ibb.co/fNynw6y/wwe.png";
+    const OG_ID = "dv1611";
+    
