e6803f1fa8867b08b25092eaba891ef185f3b5fd
Download2play <download2play.help@gmail.com>
2024-09-12 12:18:03 +0545

    Automated commit via script

---
+++ marvel_spider-man_2/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Marvel Spider-Man 2";
+    const IMAGE_URL = "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2023/06/39-the-most-exciting-thing-about-spider-man-2.jpg";
+    const GAME_LOGO = "https://cdn.marvel.com/content/1x/marvelsspiderman2_lob_crd_02.jpg";
+    const OG_ID = "r7vdnd";
+    
