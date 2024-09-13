6482fdab2fc8885ad7dfeb7eb2fb4b1329458a04
Download2play <download2play.help@gmail.com>
2024-09-13 09:46:20 +0545

    Automated commit via script

---
+++ ghost_of_tsushima/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Ghost of Tsushima";
+    const IMAGE_URL = "https://press-start.com.au/wp-content/uploads/2020/07/Ghost-of-Tsushima-Review-.jpg";
+    const GAME_LOGO = "https://image.api.playstation.com/vulcan/ap/rnd/202010/0222/b3iB2zf2xHj9shC0XDTULxND.png";
+    const OG_ID = "42nqwx";
+    
