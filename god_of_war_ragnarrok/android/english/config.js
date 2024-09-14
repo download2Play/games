2db743b351ef9fddbd15a329d22e1bb8516975ad
Download2play <download2play.help@gmail.com>
2024-09-14 15:25:45 +0545

    Automated commit via script

---
+++ god_of_war_ragnarrok/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "God of War Ragnarrok";
+    const IMAGE_URL = "https://cdn.mos.cms.futurecdn.net/Kpyrce3HohJrWopmPDmTMW.jpg";
+    const GAME_LOGO = "https://upload.wikimedia.org/wikipedia/en/e/ee/God_of_War_Ragnar%C3%B6k_cover.jpg";
+    const OG_ID = "o6kx7n";
+    
