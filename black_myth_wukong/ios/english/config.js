b33de3c895b6979899334fed2da589fe557245f6
Download2play <download2play.help@gmail.com>
2024-09-15 16:28:03 +0545

    Automated commit via script

---
+++ black_myth_wukong/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Black Myth Wukong";
+    const IMAGE_URL = "https://www.gamersyde.com/newsv3/black_myth_wukong_cg_trailer-23725.jpg";
+    const GAME_LOGO = "https://cdn2.steamgriddb.com/grid/9ac2641162c8716cdf59a40367e21b6e.png";
+    const OG_ID = "37kogw";
+    
