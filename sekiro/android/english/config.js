6985ac6940208afe7301d9bd4a99d6862f18e791
Download2play <download2play.help@gmail.com>
2024-09-21 20:23:58 +0545

    Automated commit via script

---
+++ sekiro/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Sekiro";
+    const IMAGE_URL = "https://assets.rockpapershotgun.com/images/2018/08/Sekiro_07.jpg";
+    const GAME_LOGO = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/3367fa99-0ba6-454c-b947-683f1a9f896d/ddwh3ph-92b34e9e-b5fb-4507-a8e1-1253c19b21b9.png";
+    const OG_ID = "lk1kjg";
+    
