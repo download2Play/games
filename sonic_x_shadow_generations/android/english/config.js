09f5be32ce17b3e11f0843e9203bf45bf2dc8db0
Ranjan Shrestha <76832884+runzun1987@users.noreply.github.com>
2024-10-26 08:13:29 +0545

    Automated commit via script

---
+++ sonic_x_shadow_generations/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Sonic X Shadow Generations";
+    const IMAGE_URL = "https://i.ytimg.com/vi/6lw7EAV-by4/maxresdefault.jpg";
+    const GAME_LOGO = "https://sm.ign.com/ign_za/cover/s/sonic-x-sh/sonic-x-shadow-generations_e23x.jpg";
+    const OG_ID = "8dem8d";
+    
