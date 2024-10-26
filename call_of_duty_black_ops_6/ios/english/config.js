99ccf1442086c212afa390d74632bdf03a98eb5a
Ranjan Shrestha <76832884+runzun1987@users.noreply.github.com>
2024-10-26 08:02:36 +0545

    Automated commit via script

---
+++ call_of_duty_black_ops_6/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Call of Duty Black Ops 6";
+    const IMAGE_URL = "https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blackops6/meta/BO6_LP-meta_image.jpg";
+    const GAME_LOGO = "https://i.ibb.co/D1f0L2c/dhkumrz-de530af0-27dc-407e-8661-5654e9445454.png";
+    const OG_ID = "lk1oww";
+    
