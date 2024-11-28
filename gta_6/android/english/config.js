34d3f1cbeb567c4a6d8cd0dca9a8e2aa67a1d63d
Ranjan Shrestha <bt.news4u@gmail.com>
2024-11-28 10:14:23 +0545

    Automated commit via script

---
+++ gta_6/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "GTA 6";
+    const IMAGE_URL = "https://media-rockstargames-com.akamaized.net/mfe6/prod/__common/img/71d4d17edcd49703a5ea446cc0e588e6.jpg";
+    const GAME_LOGO = "https://images.seeklogo.com/logo-png/51/1/gta-vi-logo-png_seeklogo-511746.png";
+    const OG_ID = "qkdr16";
+    
