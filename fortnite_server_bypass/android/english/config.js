b1a4f48b5ade8dfd4221276219b2b6fe43daae46
Ranjan Shrestha <bt.news4u@gmail.com>
2024-12-10 16:43:03 +0545

    Automated commit via script

---
+++ fortnite_server_bypass/android/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Fortnite Server Bypass";
+    const IMAGE_URL = "https://image.api.playstation.com/vulcan/ap/rnd/202410/2918/95953c3726f54fba5e6cf53f97b10bcf99e0d43581ae2c55.jpg";
+    const GAME_LOGO = "https://crystalpng.com/wp-content/uploads/2024/09/Fortnite-logo-png.png";
+    const OG_ID = "42nn61";
+    
