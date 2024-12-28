db9b243dae44848d26601f5f68c2eae2777d6569
Ranjan Shrestha <76832884+runzun1987@users.noreply.github.com>
2024-12-28 17:10:24 +0545

    Automated commit via script

---
+++ freedom_wars_remastered/ios/english/config.js
@@ -0,0 +1,6 @@
+
+    const WEB_TITLE = "Freedom Wars Remastered";
+    const IMAGE_URL = "https://static.bandainamcoent.eu/high/freedom-wars/00-page-setup/fw-video-thumb.jpg";
+    const GAME_LOGO = "https://assets-prd.ignimgs.com/2024/09/18/freedom-wars-remastered-button-1726666927984.jpg";
+    const OG_ID = "r7m79l";
+    
